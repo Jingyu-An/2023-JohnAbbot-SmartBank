b69ccd9f0a74c4b42a8b57137b59ef8e4468a6ee
Pat <la_spirou@hotmail.com>
2023-01-17 15:20:38 -0500

    first commit of basic files for project smart app

---
+++ Day07AzureDb/SmartBankingDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Day07AzureDb
+{
+    public partial class SmartBankingDbContext : DbContext
+    {
+        public SmartBankingDbContext()
+            : base("name=SmartBankingDbContext")
+        {
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
