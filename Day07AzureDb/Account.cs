a3a1c90312b7808d7c4565707535007a09718757
Pat <la_spirou@hotmail.com>
2023-01-18 22:48:21 -0500

    created Account and Customer pages

---
+++ Day07AzureDb/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07AzureDb
+{
+    public class Account
+    {
+        public Account() { }
+
+        public Account(int customer_id, int user_id, string bank_branch_address, string phone_number_branch)
+        {
+            Customer_id = customer_id;
+            User_id = user_id;
+            Bank_branch_address = bank_branch_address;
+            Phone_number_branch = phone_number_branch;
+        }
+
+        int Id { get; set; }
+        
+        public int Customer_id { get; set; }
+        public int User_id { get; set; }
+        public string Bank_branch_address{ get; set; }
+        public string Phone_number_branch { get; set; }
+
+
+
+    }
+}
---
+++ Day07AzureDb/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07AzureDb
+{
+    public class Customer
+    {
+        public Customer () { }
+
+        public Customer(string email, string full_name, int phone_number, string password, string address, DateTime created_at, string account_type)
+        {
+            Email = email;
+            Full_name = full_name;
+            Phone_number = phone_number;
+            Password = password;
+            Address = address;
+            Created_at = created_at;
+            Account_type = account_type;
+        }
+
+        [Key]
+        public int Customer_id { get; set; }
+        public string Email { get; set; }
+        public string Full_name { get; set; }
+        public int Phone_number { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public DateTime Created_at { get; set; }
+        public string Account_type { get; set; }
+    }
+}
