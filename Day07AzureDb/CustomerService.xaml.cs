5608be4a6a8590147f445c0528ed1453c9464548
Jingyu-An <103467264+Jingyu-An@users.noreply.github.com>
2023-01-19 22:54:26 -0500

    [Customer Service] : Add sending mail functions

--- Day07AzureDb/CustomerService.xaml.cs
+++ Day07AzureDb/CustomerService.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,75 @@ namespace Day07AzureDb
         public CustomerService()
         {
             InitializeComponent();
+        }
+
+        private void BtnAttatchFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog().GetValueOrDefault())
+            {
+                LblFileName.Content = openFileDialog.FileName;
+            }
+        }
+
+        private void BtnSend_Click(object sender, RoutedEventArgs e)
+        {
+            string smtpAddress = "smtp.google.com";
+            int smtpPort = 587;
+            bool smtpSSL = true;
+            string smtpId = "";
+            string smtpPw = "";
+            bool isBodyHtml = false;
+
+            using (SmtpClient smtp = new SmtpClient(smtpAddress, smtpPort))
+            {
+                smtp.TargetName = smtpAddress;
+                smtp.EnableSsl = smtpSSL;
+                smtp.Credentials = new NetworkCredential(smtpId, smtpPw);
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                using (MailMessage mail = new MailMessage())
+                {
+                    try
+                    {
+                        mail.From = new MailAddress(TbxEmail.Text);
+                        mail.To.Add("jqrohan@gmail.com");
+                        mail.SubjectEncoding = Encoding.UTF8;
+                        mail.Subject = TbxSubject.Text;
+
+                        mail.BodyEncoding = Encoding.UTF8;
+                        mail.Body = $" Name: {TbxName.Text}\n Phone: {TbxPhone.Text}\n" +
+                            StringFromRichTextBox(RTBBody);
+
+                        mail.IsBodyHtml = isBodyHtml;
+
+                        System.Net.Mail.Attachment attachment;
+                        attachment = new System.Net.Mail.Attachment(LblFileName.Content.ToString());
+                        mail.Attachments.Add(attachment);
+
+                        smtp.Send(mail);
+                        MessageBox.Show("Sending mail ok", "Mail sending successfull", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    catch (Exception ex) when (ex is SmtpException | ex is ArgumentException) 
+                    {
+                        MessageBox.Show("Fail sending mail: " + ex.Message, "Mail sending fail", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+        }
+
+        string StringFromRichTextBox(RichTextBox rtb)
+        {
+            TextRange textRange = new TextRange(
+                // TextPointer to the start of content in the RichTextBox.
+                rtb.Document.ContentStart,
+                // TextPointer to the end of content in the RichTextBox.
+                rtb.Document.ContentEnd
+            );
+
+            // The Text property on a TextRange object returns a string
+            // representing the plain text content of the TextRange.
+            return textRange.Text;
         }
     }
 }
